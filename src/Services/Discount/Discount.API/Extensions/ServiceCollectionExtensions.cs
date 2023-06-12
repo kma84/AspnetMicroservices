89ebe88b38aac4fdce9d4ba7f36ea6991fb4e8fe
kma84 <sergio.kma@gmail.com>
2023-06-12 17:51:32 +0200

    (WIP) Seed postgre database data

---
+++ src/Services/Discount/Discount.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Discount.API.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+
+        public static IServiceCollection MigrateDatabase<TContext>(this IServiceCollection services, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            using (var scope = services.BuildServiceProvider().CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+                var logger = serviceProvider.GetRequiredService<ILogger<TContext>>();
+
+            }
+
+            return services;
+        }
+
+    }
+}
