5648d6a598c7decd448ee3a630c4f3cb42e62e3c
kma84 <sergio.kma@gmail.com>
2023-06-13 17:30:54 +0200

    Seed postgre database data

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,3 +1,4 @@
+using Discount.API.Extensions;
 using Discount.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.MigrateDatabase<Program>();
 
 var app = builder.Build();
 
