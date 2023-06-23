952f63228d711696b7b87f9971f3250e96681300
kma84 <sergio.kma@gmail.com>
2023-06-23 14:17:54 +0200

    Connection to Grpc service form Basket.API

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -28,6 +28,8 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
+            // TODO: llamar a servicio grpc
+
             return Ok(await _repository.UpdateBasket(basket));
         }
 
